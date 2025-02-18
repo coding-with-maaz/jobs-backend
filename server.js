baa15df8a8cb4422dffedfe74f3adbc82501d969
Muhammad Maaz <127884486+coding-with-maaz@users.noreply.github.com>
2025-02-19 02:35:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, '0.0.0.0', () => {
+    app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
