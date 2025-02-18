461c720374cc1fc3ee11f481b83f962b8cfb2435
Muhammad Maaz <127884486+coding-with-maaz@users.noreply.github.com>
2025-02-19 02:59:30 +0500

    Update jobs.js

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,24 +1,18 @@
+// routes/jobs.js
 const express = require('express');
 const router = express.Router();
-const {
-  validateJob,
-  getAllJobs,
-  getJobById,
-  createJob,
-  updateJob,
-  deleteJob
-} = require('../controllers/jobController');
+const jobController = require('../controllers/jobController');
 const adConfigController = require('../controllers/adConfigController');
 
-// Routes
-router.get('/', getAllJobs);
-router.get('/:id', getJobById);
-router.post('/', validateJob, createJob);
-router.put('/:id', validateJob, updateJob);
-router.delete('/:id', deleteJob);
+// Job-related routes
+router.get('/', jobController.getAllJobs);
+router.get('/:id', jobController.getJobById);
+router.post('/', jobController.createJob);
+router.put('/:id', jobController.updateJob);
+router.delete('/:id', jobController.deleteJob);
 
+// Ad configuration routes
 router.get('/adConfig', adConfigController.getAdConfig);
 router.put('/adConfig', adConfigController.updateAdConfig);
-
 
 module.exports = router;
