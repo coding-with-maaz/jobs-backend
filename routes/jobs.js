c9d017824a1f079252b413e325d8883cb1d88df4
Muhammad Maaz <127884486+coding-with-maaz@users.noreply.github.com>
2025-02-19 02:21:36 +0500

    Update jobs.js

--- routes/jobs.js
+++ routes/jobs.js
@@ -16,4 +16,7 @@ router.post('/', validateJob, createJob);
 router.put('/:id', validateJob, updateJob);
 router.delete('/:id', deleteJob);
 
+router.get('/adConfig', adConfigController.getAdConfig); // Fetch ad configuration
+
+
 module.exports = router;
