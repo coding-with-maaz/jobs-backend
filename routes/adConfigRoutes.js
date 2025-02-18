827ebaaa2dec336c345d898f2cf22219d80d38d4
Muhammad Maaz <127884486+coding-with-maaz@users.noreply.github.com>
2025-02-19 02:47:48 +0500

    Update adConfigRoutes.js

--- routes/adConfigRoutes.js
+++ routes/adConfigRoutes.js
@@ -1,7 +1,7 @@
 // routes/adConfigRoutes.js
 const express = require('express');
 const router = express.Router();
-const adConfigController = require('../controllers/adConfigController');
+const adConfigController = require('../controllers/AdConfigController');
 
 router.get('/', adConfigController.getAdConfig);
 router.put('/', adConfigController.updateAdConfig);
